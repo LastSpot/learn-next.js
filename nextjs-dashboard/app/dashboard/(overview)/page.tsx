ca6ca41b7ea3af33eac78acf5e3c9967ea2578b7
Michael <46937389+LastSpot@users.noreply.github.com>
2025-04-24 23:40:47 -0400

    Refactor code structure for improved readability and maintainability

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -14,6 +14,11 @@ import {
   // fetchCardData 
 } from '@/app/lib/data';
 import CardWrapper from '@/app/ui/dashboard/cards';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
  
 export default async function Page() {
   // const revenue = await fetchRevenue();
